f17ef0d6eb1432e89f15008f6bfde6e89c424876
Danielip1705 <daniel.iglesias@iesnervion.es>
2023-10-06 14:28:45 +0200

    CORRECCION

--- src/tema2/Ejercicio9.java
+++ src/tema2/Ejercicio9.java
@@ -6,20 +6,28 @@ public class Ejercicio9 {
 
 	public static void main(String[] args) {
 		//
-		int piedra = 1;
-		int papel = 2;
-		int tijeras = 3;
-		int num;
+		final String PIEDRA = "PIEDRA";
+		final String PAPEL = "PAPEL";
+		final String TIJERAS = "TIJERAS";
+		String jugador1;
+		String jugador2;
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Jugador 1 elija Piedra [1] Papel [2] Tijeras [3]");
+		System.out.println("Jugador 1 Eliga [PIEDRA] [TIJERAS] o [PAPEL]");
 		
-		num = sc.nextInt();
+		jugador1 = sc.next();
 		
-		System.out.println("Jugador 1 elija Piedra [1] Papel [2] Tijeras [3]");
+		System.out.println("Jugador 2 Eliga [PIEDRA] [TIJERAS] o [PAPEL]");
 		
+		jugador2 = sc.next();
 		
-		
+		if (jugador1==PIEDRA&&jugador2==PIEDRA) {
+			System.out.println("Empate");
+		} else if (jugador1==PAPEL&&jugador2==PAPEL) {
+			System.out.println("Empate");
+		} else if (jugador1==TIJERAS&&jugador2==TIJERAS) {
+			System.out.println("Empate");
+		}
 		
 		
 
